4e7fec5522cac551b8926c607e408d181e6f340c
ordercomplete <ordercomplete777@gmail.com>
2024-02-01 19:07:20 +0200

    first commit

--- src/route/index.js
+++ src/route/index.js
@@ -3,10 +3,100 @@ const router = express.Router()
 
 router.get('/', function (req, res) {
   res.render('index', {
-    layout: 'default',
-    title: 'Home page',
-    text: 'Click me',
+    user: {
+      name: 'Іван',
+      email: 'ivan@example.com',
+      phone: '123-456-7890',
+      balance: 0,
+    },
   })
 })
+
+// router.get('/', function (req, res) {
+//   res.render('index', {
+//     product: {
+//       name: 'Корм для риб',
+//       cost: 72.5,
+//       img: 'fish.png',
+//       isAvailable: false,
+//     },
+//   })
+// })
+
+// router.get('/', function (req, res) {
+//   res.render('index', {
+//     product: {
+//       name: 'Корм для риб',
+//       cost: 72.5,
+//       img: 'fish.png',
+//       isAvailable: true,
+//     },
+//   })
+// })
+
+// router.get('/', function (req, res) {
+//   res.render('index', {
+//     product: {
+//       name: 'Корм для риб',
+//       cost: 72.5,
+//       img: 'fish.png',
+//       isAvailable: true,
+//     },
+//   })
+// })
+
+// router.get('/', function (req, res) {
+//   res.render('index', {
+//     user: {
+//       name: 'Іван',
+//       email: 'ivan@example.com',
+//       phone: '123-456-7890',
+//     },
+//   })
+// })
+
+// router.get('/', function (req, res) {
+//   res.render('index', {
+//     product: {
+//       name: 'Корм для риб',
+//       cost: 72.5,
+//       img: 'fish.png',
+//     },
+//   })
+// })
+
+// router.get('/users', function (req, res) {
+//   res.render('user-list', {
+//     layout: 'header',
+//     title: 'User list page',
+//     // text: 'Click me',
+
+//     data: {
+//       userlist: [
+//         {
+//           id: 5123,
+//           name: `Ivan`,
+//           isAdmin: true,
+//           isConfirm: true,
+//           balance: 100,
+//         },
+//         {
+//           id: 6843,
+//           name: `Dima`,
+//           isAdmin: false,
+//           isConfirm: false,
+//           balance: 0,
+//         },
+//         {
+//           id: 7745,
+//           name: `Vasya`,
+//           isAdmin: true,
+//           isConfirm: true,
+//           balance: null,
+//         },
+//       ],
+//     },
+//   })
+// })
 
 module.exports = router
